8899705fde0ca9cb8f82996c9800dc19d13d093b
jlgerber <jlgerber@gmail.com>
2020-07-23 17:45:36 -0700

    refactors build::construct_command - adding helpers

--- src/targets/build.rs
+++ src/targets/build.rs
@@ -31,22 +31,56 @@ impl Doit for Build {
         }
         Ok(())
     }
+
     /// construct the command which will be executed
     fn construct_command(&self) -> Result<String, Self::Err> {
         let build_env = BuildEnv::new(".")?;
-        // get the build dir from the build_env
-        let env_dist_dir = build_env
-            .dist_dir
-            .to_str()
-            .ok_or(anyhow!("unable to fetch dist_dir from env"))?
-            .into();
 
+        let defines_str = self.get_defines_str();
+
+        // if the use supplied the dist_dir, great. Otherwise, grab it from the env
+        let dist_dir_str = self.get_dist_dir_str(&build_env)?;
+
+        let docs_str = if self.with_docs { " --with-docs" } else { "" };
+
+        let flavor_str = self.get_flavor_str();
+
+        if self.verbose {
+            println!(
+                "dist_dir: '{}' docs_str: '{}' flavor_str: '{}' defines_str: '{}'\n",
+                &dist_dir_str, &docs_str, &flavor_str, &defines_str
+            );
+        }
+        let result = format!(
+            "pk audit && pk build {}{}{}{}",
+            dist_dir_str, docs_str, flavor_str, defines_str
+        );
+        Ok(result)
+    }
+}
+
+// implementation of private convenience methods
+impl Build {
+    fn get_defines_str(&self) -> String {
+        // NB: The -D flag works differently in pk build in that it
+        // follows posix convention for multiple values; it supports
+        // multiple invocations of the flag.
         let mut defines_str = String::new();
         if self.defines.is_some() {
             for def in self.defines.as_ref().unwrap() {
                 defines_str.push_str(&format!(" -D={}", def));
             }
         }
+        defines_str
+    }
+
+    fn get_dist_dir_str(&self, build_env: &BuildEnv) -> Result<String, AnyError> {
+        let env_dist_dir = build_env
+            .dist_dir
+            .to_str()
+            .ok_or(anyhow!("unable to fetch dist_dir from env"))?
+            .into();
+
         // if the use supplied the dist_dir, great. Otherwise, grab it from the env
         let dist_dir = self.dist_dir.as_ref().unwrap_or(&env_dist_dir);
         let dist_dir_str = if self.dist_dir.is_some() {
@@ -54,6 +88,10 @@ impl Doit for Build {
         } else {
             "".to_string()
         };
+        Ok(dist_dir_str)
+    }
+
+    fn get_flavor_str(&self) -> String {
         // wow this one is fun. we need to convert Option<T> -> Option<&T> then unwrap,
         // get a vector of Flavors, them convert them to strs, and join them into a string
         let flavor = if self.flavors.is_some() {
@@ -69,23 +107,12 @@ impl Doit for Build {
         } else {
             "".to_string()
         };
-        let docs_str = if self.with_docs { " --with-docs" } else { "" };
         let flavor_str = if self.flavors.is_some() {
             format!(" --flavor={}", &flavor)
         } else {
             "".to_string()
         };
-        if self.verbose {
-            println!(
-                "dist_dir: '{}' docs_str: '{}' flavor_str: '{}' defines_str: '{}'",
-                &dist_dir_str, &docs_str, &flavor_str, &defines_str
-            );
-        }
-        let result = format!(
-            "pk audit && pk build {}{}{}{}",
-            dist_dir_str, docs_str, flavor_str, defines_str
-        );
-        Ok(result)
+        flavor_str
     }
 }
 
@@ -102,17 +129,24 @@ impl Default for Build {
     }
 }
 
+// Implementation of the Setter methods
 impl Build {
+    /// Set the with_docs value and return a mutable reference to self per the
+    /// builder pattern.
     pub fn with_docs(&mut self, value: bool) -> &mut Self {
         self.with_docs = value;
         self
     }
 
+    /// Set the dry_run value and return a mutable reference to self
+    /// as per the builder pattern.
     pub fn dry_run(&mut self, value: bool) -> &mut Self {
         self.dry_run = value;
         self
     }
 
+    /// Set the dist_dir value and return a mutable reference to
+    /// self, per the builder pattern.
     pub fn dist_dir<I>(&mut self, input: Option<I>) -> &mut Self
     where
         I: Into<String>,
@@ -123,7 +157,7 @@ impl Build {
         }
         self
     }
-    /// insert flavors.
+    /// Set flavors per the builder pattern
     pub fn flavors(&mut self, input: Option<Vec<Flavor>>) -> &mut Self {
         match input {
             None => self.flavors = None,
@@ -141,11 +175,14 @@ impl Build {
         }
         self
     }
-    /// Set verbose state
+    /// Set verbose state and return a mutable reference to self
+    /// per the builder pattern.
     pub fn verbose(&mut self, input: bool) -> &mut Self {
         self.verbose = input;
         self
     }
+    /// Set the defines and return a mutable reference to self per the
+    /// builder pattern.
     pub fn defines(&mut self, input: Option<Vec<String>>) -> &mut Self {
         self.defines = input;
         self
