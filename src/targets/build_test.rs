d907fea3ff2e088f34e5acebb5de0862722ff8cd
jlgerber <jlgerber@gmail.com>
2020-07-25 11:54:40 -0700

    moves build tests to separate file

---
+++ src/targets/build_test.rs
@@ -0,0 +1,189 @@
+use super::*;
+
+#[test]
+pub fn can_construct_default() {
+    let result = Build::default();
+    let expected = Build {
+        clean: false,
+        with_docs: true,
+        dry_run: false,
+        dist_dir: None,
+        flavors: None,
+        level: None,
+        metadata_only: false,
+        overrides: None,
+        platforms: None,
+        verbose: false,
+        defines: None,
+        work: false,
+    };
+    assert_eq!(result, expected);
+}
+
+#[test]
+pub fn can_set_with_docs() {
+    let mut result = Build::default();
+    result.with_docs(false);
+    let expected = Build {
+        clean: false,
+        with_docs: false, // set by with_docs above
+        dry_run: false,
+        dist_dir: None,
+        flavors: None,
+        level: None,
+        metadata_only: false,
+        overrides: None,
+        platforms: None,
+        verbose: false,
+        defines: None,
+        work: false,
+    };
+    assert_eq!(result, expected);
+}
+
+#[test]
+pub fn can_set_dry_run() {
+    let mut result = Build::default();
+    result.dry_run(true);
+    let expected = Build {
+        clean: false,
+        with_docs: true, // set by with_docs above
+        dry_run: true,
+        dist_dir: None,
+        flavors: None,
+        level: None,
+        metadata_only: false,
+        overrides: None,
+        platforms: None,
+        verbose: false,
+        defines: None,
+        work: false,
+    };
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn can_set_dist_dir() {
+    let mut result = Build::default();
+    result.dist_dir(Some("foo/bar"));
+    let expected = Build {
+        clean: false,
+        with_docs: true, // set by with_docs above
+        dry_run: false,
+        dist_dir: Some("foo/bar".to_string()),
+        flavors: None,
+        level: None,
+        metadata_only: false,
+        overrides: None,
+        platforms: None,
+        verbose: false,
+        defines: None,
+        work: false,
+    };
+    assert_eq!(result, expected);
+    // now test it with a String
+    let mut result = Build::default();
+    result.dist_dir(Some("foo/bar".to_string()));
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn can_set_flavors() {
+    let mut result = Build::default();
+    result.flavors(Some(vec![
+        Flavor::Vanilla,
+        Flavor::Named("foo".to_string()),
+    ]));
+    let mut flavs = HashSet::new();
+    flavs.insert(Flavor::Vanilla);
+    flavs.insert(Flavor::Named("foo".to_string()));
+    let expected = Build {
+        clean: false,
+        with_docs: true, // set by with_docs above
+        dry_run: false,
+        dist_dir: None,
+        flavors: Some(flavs),
+        level: None,
+        metadata_only: false,
+        overrides: None,
+        platforms: None,
+        verbose: false,
+        defines: None,
+        work: false,
+    };
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn setting_flavors_none_clears() {
+    let mut result = Build::default();
+    result.flavors(Some(vec![
+        Flavor::Vanilla,
+        Flavor::Named("foo".to_string()),
+    ]));
+    result.flavors(None);
+    let expected = Build {
+        clean: false,
+        with_docs: true, // set by with_docs above
+        dry_run: false,
+        dist_dir: None,
+        flavors: None,
+        level: None,
+        metadata_only: false,
+        overrides: None,
+        platforms: None,
+        verbose: false,
+        defines: None,
+        work: false,
+    };
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn can_set_verbose() {
+    let mut result = Build::default();
+    result.verbose(true);
+    let expected = Build {
+        clean: false,
+        with_docs: true,
+        dry_run: false,
+        dist_dir: None,
+        flavors: None,
+        level: None,
+        metadata_only: false,
+        overrides: None,
+        platforms: None,
+        verbose: true,
+        defines: None,
+        work: false,
+    };
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn can_build() {
+    let result = Build::default()
+        .with_docs(false)
+        .dry_run(true)
+        .dist_dir(Some("foo/bar"))
+        .flavors(Some(vec![Flavor::Vanilla]))
+        .verbose(true)
+        .build();
+    let mut flavs = HashSet::new();
+    flavs.insert(Flavor::Vanilla);
+    let expected = Build {
+        clean: false,
+        with_docs: false, // set by with_docs above
+        dry_run: true,
+        dist_dir: Some("foo/bar".to_string()),
+        flavors: Some(flavs),
+        level: None,
+        metadata_only: false,
+        overrides: None,
+        platforms: None,
+        verbose: true,
+        defines: None,
+        work: false,
+    };
+    assert_eq!(result, expected);
+}
