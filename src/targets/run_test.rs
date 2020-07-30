929f5c88db5fa39abb7f6c002699d3f64cc5ad25
Jonathan Gerber <jlgerber@gmail.com>
2020-07-30 12:09:51 -0700

    updates tests for Run target

--- src/targets/run_test.rs
+++ src/targets/run_test.rs
@@ -2,7 +2,6 @@ use super::*;
 use serial_test::serial;
 use crate::utils::setup_manifest_dir;
 use std::env;
-use indexmap::IndexSet as HashSet;
 
 
 #[test]
@@ -24,7 +23,7 @@ fn can_update_build() {
     let result = Run::default()
         .dry_run(true)
         .verbose(true)
-        .vars(vec!["foo".into(), "bar".into()])
+        .vars(vec!["foo", "bar"])
         .build();
     let expected = Run {
         dry_run: true,
@@ -32,7 +31,7 @@ fn can_update_build() {
         package_root: None,
         flavors: None,
         platforms: None,
-        vars: vec!["foo".into(), "bar".into()],
+        vars: vec!["foo", "bar"].into_iter().map(|x| x.to_string()).collect::<Vec<_>>(),
     };
     assert_eq!(result, expected);
 }
@@ -41,7 +40,7 @@ fn propagates_dry_run_and_verbose_to_vars_if_set() {
     let mut result = Run::default()
         .dry_run(true)
         .verbose(true)
-        .vars(vec!["foo".into(), "bar".into()])
+        .vars(vec!["foo", "bar"])
         .build();
     result.fix_args().unwrap();
     let expected = Run {
@@ -51,11 +50,11 @@ fn propagates_dry_run_and_verbose_to_vars_if_set() {
         flavors: None,
         platforms: None,
         vars: vec![
-            "foo".into(),
-            "bar".into(),
-            "--verbose".into(),
-            "--dry-run".into(),
-        ],
+            "foo",
+            "bar",
+            "--verbose",
+            "--dry-run",
+        ].into_iter().map(|x| x.to_string()).collect::<Vec<_>>(),
     };
     assert_eq!(result, expected);
 }
@@ -65,10 +64,10 @@ fn propagates_dry_run_and_verbose_to_struct_if_set_in_vars() {
         .dry_run(false)
         .verbose(false)
         .vars(vec![
-            "foo".into(),
-            "bar".into(),
-            "--verbose".into(),
-            "--dry-run".into(),
+            "foo",
+            "bar",
+            "--verbose",
+            "--dry-run",
         ])
         .build();
     result.fix_args().unwrap();
@@ -79,11 +78,11 @@ fn propagates_dry_run_and_verbose_to_struct_if_set_in_vars() {
         flavors: None,
         platforms: None,
         vars: vec![
-            "foo".into(),
-            "bar".into(),
-            "--verbose".into(),
-            "--dry-run".into(),
-        ],
+            "foo",
+            "bar",
+            "--verbose",
+            "--dry-run",
+        ].into_iter().map(|x| x.to_string()).collect::<Vec<_>>(),
     };
     assert_eq!(result, expected);
 }
@@ -92,7 +91,7 @@ fn propagates_dry_run_and_verbose_to_struct_if_set_in_vars_as_short_flags() {
     let mut result = Run::default()
         .dry_run(false)
         .verbose(false)
-        .vars(vec!["foo".into(), "-n".into(), "-v".into(), "bar".into()])
+        .vars(vec!["foo", "-n", "-v", "bar"])
         .build();
     result.fix_args().unwrap();
     let expected = Run {
@@ -101,16 +100,18 @@ fn propagates_dry_run_and_verbose_to_struct_if_set_in_vars_as_short_flags() {
         package_root: None,
         flavors: None,
         platforms: None,
-        vars: vec!["foo".into(), "-n".into(), "-v".into(), "bar".into()],
+        vars: vec!["foo", "-n", "-v", "bar"].into_iter().map(|x| x.to_string()).collect::<Vec<_>>(),
     };
     assert_eq!(result, expected);
 }
+
+
 #[test]
 fn propagates_verbose_to_struct_if_set_in_vars_as_short_flags() {
     let mut result = Run::default()
         .dry_run(false)
         .verbose(false)
-        .vars(vec!["foo".into(), "-v".into(), "bar".into()])
+        .vars(vec!["foo", "-v", "bar"].into_iter().map(|x| x.to_string()).collect::<Vec<_>>())
         .build();
     result.fix_args().unwrap();
     let expected = Run {
@@ -119,7 +120,7 @@ fn propagates_verbose_to_struct_if_set_in_vars_as_short_flags() {
         package_root: None,
         flavors: None,
         platforms: None,
-        vars: vec!["foo".into(), "-v".into(), "bar".into()],
+        vars: vec!["foo", "-v", "bar"].into_iter().map(|x| x.to_string()).collect::<Vec<_>>(),
     };
     assert_eq!(result, expected);
 }
@@ -129,7 +130,7 @@ fn propagates_dry_run_to_struct_if_set_in_vars_as_short_flags() {
     let mut result = Run::default()
         .dry_run(false)
         .verbose(false)
-        .vars(vec!["foo".into(), "-n".into(), "bar".into()])
+        .vars(vec!["foo", "-n", "bar"])
         .build();
     result.fix_args().unwrap();
     let expected = Run {
@@ -138,14 +139,14 @@ fn propagates_dry_run_to_struct_if_set_in_vars_as_short_flags() {
         package_root: None,
         flavors: None,
         platforms: None,
-        vars: vec!["foo".into(), "-n".into(), "bar".into()],
+        vars: vec!["foo", "-n", "bar"].into_iter().map(|x| x.to_string()).collect::<Vec<_>>(),
     };
     assert_eq!(result, expected);
 }
 
 #[test]
 fn doit_errors_if_first_vars_is_a_flag() {
-    let mut result = Run::default().vars(vec!["-v".into(), "bla".into()]).build();
+    let mut result = Run::default().vars(vec!["-v", "bla"]).build();
     let result = result.doit();
     assert!(result.is_err());
 }
@@ -160,3 +161,64 @@ fn doit_errors_if_vars_is_empty() {
 //
 // build_cmd
 //
+
+
+#[test]
+#[serial]
+fn build_cmd_given_default() {
+    setup_manifest_dir(false);
+    env::set_var("DD_SHOW", "DEV01");
+    env::set_var("DD_OS", "cent7_64");
+    let result = Run::default().build_cmd();
+    
+    assert!(result.is_err());
+}
+
+
+#[test]
+#[serial]
+fn build_cmd_given_build_target() -> Result<(), Box<dyn std::error::Error>> {
+    setup_manifest_dir(false);
+    env::set_var("DD_SHOW", "DEV01");
+    env::set_var("DD_OS", "cent7_64");
+    let result = Run::default()
+        .vars(vec!["foobar"])
+        .build_cmd()?;
+
+    let result = result.iter().map(|s| s.as_str()).collect::<Vec<&str>>();
+    assert_eq!(result, 
+        vec!["pk run-recipe foobar"]
+    );
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn build_cmd_given_build_target_and_additional_vars() -> Result<(), Box<dyn std::error::Error>> {
+    setup_manifest_dir(false);
+    env::set_var("DD_SHOW", "DEV01");
+    env::set_var("DD_OS", "cent7_64");
+    let result = Run::default()
+        .vars(vec!["foobar", "--bla", "foo"])
+        .build_cmd()?;
+        
+    let result = result.iter().map(|s| s.as_str()).collect::<Vec<&str>>();
+    assert_eq!(result, 
+        vec!["pk run-recipe foobar --bla foo"]
+    );
+
+    Ok(())
+}
+
+
+#[test]
+#[serial]
+fn build_cmd_given_vars_starting_with_flag() {
+    setup_manifest_dir(false);
+    env::set_var("DD_SHOW", "DEV01");
+    env::set_var("DD_OS", "cent7_64");
+    let result = Run::default().vars(vec!["--foo", "--bar"]).build_cmd();
+    
+    assert!(result.is_err());
+}
